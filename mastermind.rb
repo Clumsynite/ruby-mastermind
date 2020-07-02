46196e8b0df4649bc9841055d6ee1d9b7280f350
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-07-02 20:34:42 +0530

    Added random code generation for CodeBreaker
    Added getter&setter methods for codebreaker and codemaker classes

--- mastermind.rb
+++ mastermind.rb
@@ -2,18 +2,16 @@ puts "Welcome to Mastermind by Clumsyknight"
 
 # Class to set player role 
 class Role
-    def initialise
-        @role = role
-    end
 
-    def choose_role
+    # Set role when game starts
+    def set_role
         puts "\nPlease select your role out of the given options(1,2): \n    1. CodeMaker \n    2. CodeBreaker"
         role = gets.chomp!.to_i
         
         if role == 1
             puts "\nYou have selected CodeMaker"
             @role = 'CodeMaker'
-            set_code
+            #set_code
         elsif role == 2
             puts "\nYou have selected CodeBreaker"
             @role = 'CodeBreaker'
@@ -22,6 +20,7 @@ class Role
         end
     end
 
+    # get role (whether codebreaker or codemaker)
     def get_role
         @role
     end
@@ -29,6 +28,9 @@ end
 
 # Class to set code if role CodeMaker
 class CodeMaker
+    @code = ""
+
+    # Set code with error handling for human player
     def set_code
         puts "\nNow you need to set a code for the Codebreaker(bot) to guess in 12 turns"
         puts "\nIf the guess has the correct color in the correct poisition, it'll receive an X"
@@ -50,6 +52,7 @@ class CodeMaker
             if !code.match(/[^rgbyovw]/) 
                 if code.length==4
                     puts "Valid Code"
+                    @code = code
                     st = false
                 elsif code.length<4
                     puts "Code uses less characters"
@@ -62,13 +65,42 @@ class CodeMaker
             end
         end
     end
+
+    def get_code
+        @code
+    end
 end
 
 # Class to set code automatically if role CodeBreaker
 class CodeBreaker
-    
+    @code = ""
+
+    # Set code automatically by converting random numbers to characters  
+    def set_code 
+        arr = []
+        code = ""
+        puts "\nYou have chosen to be the new CodeBreaker!\nNow wait for me to create a code in order to crack"
+        (1..4).each{arr.push(rand(1..7))}
+        arr.each do |x|
+            case x
+                when 1 then code+='r'
+                when 2 then code+='b'
+                when 3 then code+='y'
+                when 4 then code+='g'
+                when 5 then code+='o'
+                when 6 then code+='b'
+                when 7 then code+='w'
+            end
+        end
+        @code = code
+    end
+
+    # get random code
+    def get_code
+        @code
+    end
 end
 
 r = Role.new
 cm = CodeMaker.new
-cm.set_code
+cb = CodeBreaker.new
